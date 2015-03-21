2ac723f429d9fc31e015414f5c885092478d0f38
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-03-21 15:15:18 +0100

    Remove Munin::Master::UpdateWorker::to_mul

--- lib/Munin/Master/UpdateWorker.pm
+++ lib/Munin/Master/UpdateWorker.pm
@@ -842,16 +842,6 @@ sub to_sec {
 	}
 }
 
-sub to_mul {
-	my ($base, $target) = @_;
-	my $target_sec = to_sec($target);
-	if ($target %% $base != 0) {
-		return 0;
-	}
-
-	return round($target / $base); 
-}
-
 sub to_mul_nb {
 	my ($base, $target) = @_;
 	my $target_sec = to_sec($target);

ef7f02044456d01f5b9e0e0c244eb439f9977529
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-09-10 09:24:01 +0200

    Copy "nav_groups" to model

--- lib/Munin/Master/Model.pm
+++ lib/Munin/Master/Model.pm
@@ -1,4 +1,34 @@
 package Munin::Master::Model;
 use Mojo::Base -base;
+use Scalar::Util 'weaken';
+
+sub new {
+    my $class = shift;
+    my $app = shift;
+    my $self = { app => $app };
+    weaken $self->{app};
+    return bless $self, $class;
+}
+
+sub nav_groups {
+    my $self = shift;
+
+    my $query = << 'FOO';
+SELECT
+ g.name AS NAME,
+ u.path AS URL,
+ '' AS R_PATH
+FROM
+ grp g INNER JOIN
+ url u ON u.id = g.id AND u.type = 'group'
+WHERE g.p_id IS NULL
+ORDER BY g.name ASC
+FOO
+
+    my $sth = $self->{app}->db->prepare_cached($query);
+    $sth->execute();
+    my $rootgroups = $sth->fetchall_arrayref( {} );
+    return $rootgroups;
+}
 
 1;

a8800b2201ac125c992048ec9374da4a5472f1c5
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-09-11 09:05:23 +0200

    Move the fetch subs to Model.pm

--- lib/Munin/Master/Controller/HTML.pm
+++ lib/Munin/Master/Controller/HTML.pm
@@ -1,8 +1,6 @@
 package Munin::Master::Controller::HTML;
 use Mojo::Base 'Mojolicious::Controller';
 
-use Munin::Master::Model;
-
 # For now: Find the top groups, then query each of these with a
 # recursive query.
 #
@@ -16,14 +14,14 @@ use Munin::Master::Model;
 
 sub welcome {
     my $self           = shift;
-    my $nav_problems   = $self->_nav_problems();
-    my $nav_categories = $self->_nav_categories();
-    my $nav_groups     = $self->_nav_groups();
+    my $nav_problems   = $self->model->nav_problems();
+    my $nav_categories = $self->model->nav_categories();
+    my $nav_groups     = $self->model->nav_groups();
 
     my @result;
 
-    foreach my $group (@{$self->_lookup_top_groups}) {
-        push @result, $self->_lookup_group($group->{id});
+    foreach my $group (@{$self->model->top_groups}) {
+        push @result, $self->model->group($group->{id});
     }
 
     $self->render(
@@ -38,34 +36,21 @@ sub welcome {
 
 sub problems {
     my $self = shift;
+    my $nav_problems   = $self->model->nav_problems();
+    my $nav_categories = $self->model->nav_categories();
+    my $nav_groups     = $self->model->nav_groups();
 
-    my $query = << 'EOQ';
-SELECT
- nu.path AS NODEURL,
- n.name AS NODENAME,
- su.path AS URL,
- su.path AS URLX,
- s.name AS LABEL,
- d.critical AS STATE_CRITICAL,
- d.warning AS STATE_WARNING,
- d.unknown AS STATE_UNKNOWN
-FROM
- ds d
- LEFT OUTER JOIN service s ON s.id = d.service_id
- LEFT OUTER JOIN url su ON su.id = s.id and su.type = 'service'
- LEFT OUTER JOIN node n ON n.id = s.node_id
- LEFT OUTER JOIN url nu ON nu.id = n.id and nu.type = 'node'
-WHERE
- d.critical = 1 OR
- d.warning = 1 OR
- d.unknown = 1
-EOQ
+    my $problems = $self->model->problems();
 
-    my $sth = $self->db->prepare_cached($query);
-    $sth->execute();
-    my ($result) = $sth->fetchall_arrayref( {} );
+    $self->render(
+        json => {
+            nav_problems   => $nav_problems,
+            nav_categories => $nav_categories,
+            nav_groups     => $nav_groups,
+            content        => $problems,
+        }
+    );
 
-    $self->render(json => $result);
 }
 
 sub comparison {
@@ -86,109 +71,6 @@ sub service {
 sub group {
     my $self = shift;
     $self->render(text => 'group page');
-}
-
-sub _nav_problems {
-    my $self = shift;
-
-    my $query = << 'EOQ';
-SELECT
- SUM(critical) AS NCRITICALS,
- SUM(warning) AS NWARNINGS,
- SUM(unknown) AS NUNKNOWNS
-FROM ds;
-EOQ
-
-    my $sth = $self->db->prepare_cached($query);
-    $sth->execute();
-    my ($thing) = $sth->fetchall_arrayref( {} );
-    return $thing;
-}
-
-sub _nav_groups {
-    my $self = shift;
-
-    my $query = << 'FOO';
-SELECT
- g.name AS NAME,
- u.path AS URL,
- '' AS R_PATH
-FROM
- grp g INNER JOIN
- url u ON u.id = g.id AND u.type = 'group'
-WHERE g.p_id IS NULL
-ORDER BY g.name ASC
-FOO
-
-    my $sth = $self->db->prepare_cached($query);
-    $sth->execute();
-    my $rootgroups = $sth->fetchall_arrayref( {} );
-    return $rootgroups;
-}
-
-sub _nav_categories {
-    my $self = shift;
-
-    my $query = << 'EOT';
-SELECT DISTINCT category
-FROM service_categories
-ORDER BY category ASC
-EOT
-
-    my $sth = $self->db->prepare_cached($query);
-    $sth->execute();
-
-    my $globalcats = [];
-    while ( my ($_category) = $sth->fetchrow_array ) {
-        my %urls = map { ( "URL$_" => "$_category-$_.html" ) } @{$self->config->{timeperiods}};
-        push @$globalcats,
-            {
-            R_PATH => '',
-            NAME   => $_category,
-            %urls,
-            };
-    }
-    return $globalcats;
-}
-
-sub _lookup_group {
-    my $self = shift;
-    my $group_id = shift;
-
-    my $query = << 'EOQ';
-WITH RECURSIVE subgroup_of(id) AS (
- VALUES(?) UNION SELECT grp.id from grp, subgroup_of
- WHERE grp.p_id = subgroup_of.id
-)
-SELECT grp.name, grp.id, grp.p_id, node.id, node.name
-FROM
- grp LEFT JOIN
- node ON node.grp_id = grp.id
-WHERE grp.id IN subgroup_of;
-EOQ
-    my $sth = $self->db->prepare_cached($query);
-    $sth->bind_param(1, $group_id);
-    $sth->execute();
-    return $sth->fetchall_arrayref( {} );
-}
-
-sub _lookup_top_groups {
-    my $self = shift;
-
-    my $top_groups_query = <<'EOQ';
-SELECT grp.id
-FROM grp
-WHERE grp.p_id IS NULL
-EOQ
-
-    my $sth = $self->db->prepare_cached($top_groups_query);
-    $sth->execute();
-    return $sth->fetchall_arrayref({});
-}
-
-sub _lookup_categories {
-    my $self = shift;
-    return {};
 }
 
 1;
--- lib/Munin/Master/Model.pm
+++ lib/Munin/Master/Model.pm
@@ -31,4 +31,117 @@ FOO
     return $rootgroups;
 }
 
+sub nav_problems {
+    my $self = shift;
+
+    my $query = << 'EOQ';
+SELECT
+ SUM(critical) AS NCRITICALS,
+ SUM(warning) AS NWARNINGS,
+ SUM(unknown) AS NUNKNOWNS
+FROM ds;
+EOQ
+
+    my $sth = $self->{app}->db->prepare_cached($query);
+    $sth->execute();
+    my ($thing) = $sth->fetchall_arrayref( {} );
+    return $thing;
+}
+
+sub nav_categories {
+    my $self = shift;
+
+    my $query = << 'EOT';
+SELECT DISTINCT category
+FROM service_categories
+ORDER BY category ASC
+EOT
+
+    my $sth = $self->{app}->db->prepare_cached($query);
+    $sth->execute();
+
+    my $globalcats = [];
+    while ( my ($_category) = $sth->fetchrow_array ) {
+        my %urls = map { ( "URL$_" => "$_category-$_.html" ) } @{$self->{app}->config->{timeperiods}};
+        push @$globalcats,
+            {
+            R_PATH => '',
+            NAME   => $_category,
+            %urls,
+            };
+    }
+    return $globalcats;
+}
+
+sub top_groups {
+    my $self = shift;
+
+    my $top_groups_query = <<'EOQ';
+SELECT grp.id
+FROM grp
+WHERE grp.p_id IS NULL
+EOQ
+
+    my $sth = $self->{app}->db->prepare_cached($top_groups_query);
+    $sth->execute();
+    return $sth->fetchall_arrayref({});
+}
+
+sub group {
+    my $self = shift;
+    my $group_id = shift;
+
+    my $query = << 'EOQ';
+WITH RECURSIVE subgroup_of(id) AS (
+ VALUES(?) UNION SELECT grp.id from grp, subgroup_of
+ WHERE grp.p_id = subgroup_of.id
+)
+SELECT grp.name, grp.id, grp.p_id, node.id, node.name
+FROM
+ grp LEFT JOIN
+ node ON node.grp_id = grp.id
+WHERE grp.id IN subgroup_of;
+EOQ
+    my $sth = $self->{app}->db->prepare_cached($query);
+    $sth->bind_param(1, $group_id);
+    $sth->execute();
+    return $sth->fetchall_arrayref( {} );
+}
+
+sub categories {
+    my $self = shift;
+    return {};
+}
+
+sub problems {
+    my $self = shift;
+
+    my $query = << 'EOQ';
+SELECT
+ nu.path AS NODEURL,
+ n.name AS NODENAME,
+ su.path AS URL,
+ su.path AS URLX,
+ s.name AS LABEL,
+ d.critical AS STATE_CRITICAL,
+ d.warning AS STATE_WARNING,
+ d.unknown AS STATE_UNKNOWN
+FROM
+ ds d
+ LEFT OUTER JOIN service s ON s.id = d.service_id
+ LEFT OUTER JOIN url su ON su.id = s.id and su.type = 'service'
+ LEFT OUTER JOIN node n ON n.id = s.node_id
+ LEFT OUTER JOIN url nu ON nu.id = n.id and nu.type = 'node'
+WHERE
+ d.critical = 1 OR
+ d.warning = 1 OR
+ d.unknown = 1
+EOQ
+
+    my $sth = $self->{app}->db->prepare_cached($query);
+    $sth->execute();
+    my $result = $sth->fetchall_arrayref( {} );
+    return $result;
+}
+
 1;
